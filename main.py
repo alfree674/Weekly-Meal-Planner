d1761b8ac99a5cfe8c8f2b8d59f02e0092b463b6
alfree674 <alfredo060704@gmail.com>
2026-07-23 22:11:49 +0200

    Añadir y leer recetas

--- main.py
+++ main.py
@@ -30,6 +30,75 @@ def imprimir_recetas(recetas):
         
     print("\n" + "="*40 + "\n")
 
+def guardar_recetas(recetas, ruta_archivo):
+    with open(ruta_archivo, 'w', encoding='utf-8') as archivo:
+        # indent=4 para formatearlo, ensure_ascii=False para dejar tildes y ñ
+        json.dump(recetas, archivo, indent=4, ensure_ascii=False)
+
+def anadir_receta(recetas, ruta_archivo):
+    print("\n--- 🍳 AÑADIR NUEVA RECETA ---")
+    nombre = input("Nombre del plato: ").strip().capitalize()
+    #Comprobamos que no sea duplicado
+    duplicado= False
+    duplicado_id= -1
+    for receta in recetas:
+        if receta["nombre"].strip().capitalize() == nombre:
+            duplicado = True
+            duplicado_id = receta["id"]
+            break
+    if duplicado:
+        print(f"La receta '{nombre}' ya existe en tu recetario con id '{duplicado_id}'")
+        return 
+    else: #si no existe, continuamos pidiendo el resto de datos
+        #El tipo debe ser una de las opciones
+        opciones_validas = ["Desayuno", "Comida", "Merienda", "Cena"]
+        es_tipo_valido = False
+        while not es_tipo_valido:
+            tipo= input(f"Tipo de comida {opciones_validas}: ").strip().capitalize()
+            if tipo in opciones_validas:
+                es_tipo_valido = True
+            else:
+                print("Error: Por favor, escribe una de las opciones válidas.")
+    
+        #El tiempo debe ser un número  
+        es_num = False
+        while not es_num:
+            try:
+                tiempo = int(input("Introduce el tiempo de preparación: "))
+                if tiempo > 0:
+                    es_num = True
+                else:
+                    print("Debe ser mayor que cero.")
+            except ValueError:
+                print("Error: Introduce un número.")
+
+        #Pedimos los ingredientes separados por comas y lo convertimos a lista
+        ingredientes_input = input("Ingredientes (separados por coma): ")
+        ingredientes = [i.strip().capitalize() for i in ingredientes_input.split(",") if i.strip()]
+    
+        #Igual con las etiquetas
+        etiquetas_input = input("Etiquetas (separadas por coma, ej: Rápido, Tupper, Pasta): ")
+        etiquetas = [e.strip().capitalize() for e in etiquetas_input.split(",") if e.strip()]
+
+        # Calculamos el ID: CAMBIAR EN FUTURO, N-1 COMPARACIONES INNECESARIAS 
+        nuevo_id = max([r['id'] for r in recetas], default=0) + 1
+
+        nueva_receta = {
+            "id": nuevo_id,
+            "nombre": nombre,
+            "tipo": tipo,
+            "tiempo": tiempo,
+            "ingredientes": ingredientes,
+            "etiquetas": etiquetas
+        }
+
+        # Añadimos a la lista en memoria y luego guardamos en el archivo
+        recetas.append(nueva_receta)
+        guardar_recetas(recetas, ruta_archivo)
+    
+        print(f"\n✅ ¡Receta '{nombre}' añadida y guardada con éxito en el JSON!")
+
+
 
 
 
@@ -39,5 +108,23 @@ if __name__ == "__main__":
     base_datos = 'recetas.json'
     mis_recetas = cargar_recetas(base_datos)
     
-    #Mostramos por consola
-    imprimir_recetas(mis_recetas)
+    #Bucle principal para el menú interactivo)
+    while True:
+        print("\n" + "*"*40)
+        print(" 📅 ORGANIZADOR DE MENÚ SEMANAL 📅 ")
+        print("*"*40)
+        print("1. Ver todas las recetas")
+        print("2. Añadir nueva receta")
+        print("3. Salir")
+        
+        opcion = input("\nElige una opción (1-3): ")
+        
+        if opcion == '1':
+            imprimir_recetas(mis_recetas)
+        elif opcion == '2':
+            anadir_receta(mis_recetas, base_datos)
+        elif opcion == '3':
+            print("\n👋 ¡Hasta luego!")
+            break
+        else:
+            print("\n❌ Opción no válida. Por favor, elige 1, 2 o 3.")
